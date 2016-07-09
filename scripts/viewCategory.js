6fba9f87b5778c273f6337b1bb304d4c24353fba
Nandhini S <nandhini.devi1998@gmail.com>
2016-07-09 11:36:38 +0530

    Implement Categorywise search

--- scripts/viewCategory.js
+++ scripts/viewCategory.js
@@ -17,6 +17,15 @@ function addRowHandlers()
 		}(rows[i]);
 	}
 }
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
 function sortOn () {
     var t;
 	for(var i=0;i<oData.response.venues.length;i++)
@@ -32,21 +41,42 @@ function sortOn () {
 		}
 	}
 }
-		var url=window.location.search.substring(1);;
+	var Cid=getParameterByName("categoryId");
+	var name=getParameterByName("name");	
 	var client_id='10C4S0MMP2ZCTX3ACXKZ3YUSCGZXCOTXLTTOI2WVJ3WTIMH1';
 	var client_secret='T4YM5HKKRQCM1T1KQJPBMHDGPVTVBA1N3ID3NMCHIYNQDI2Q';
 	var totalResults,startindex=0,count,oData;
 	function start()
-	{	
-		var img,id,name;
-		var surl='https://api.foursquare.com/v2/venues/search?intent=browse&v=20140806&near=chennai&'+url+'&limit=50&client_id=10C4S0MMP2ZCTX3ACXKZ3YUSCGZXCOTXLTTOI2WVJ3WTIMH1&client_secret=T4YM5HKKRQCM1T1KQJPBMHDGPVTVBA1N3ID3NMCHIYNQDI2Q';
-		$.ajax(surl,{
+	{		
+		if($('#searchInCategory input').val()!='')
+			searchInCategory();
+		else
+		{
+			var surl='https://api.foursquare.com/v2/venues/search?intent=browse&v=20140806&near=chennai&categoryId='+Cid+'&limit=50&client_id=10C4S0MMP2ZCTX3ACXKZ3YUSCGZXCOTXLTTOI2WVJ3WTIMH1&client_secret=T4YM5HKKRQCM1T1KQJPBMHDGPVTVBA1N3ID3NMCHIYNQDI2Q';
+			$.ajax(surl,{
 			complete:function(p_oXHR,p_sStatus){
 					oData=$.parseJSON(p_oXHR.responseText);
 					sortOn();
+					$('#searchInCategory input').attr("placeholder","Search in "+name);
 					$('body').append('<ul id="movePage" class="pager"><li><a href="#" onClick="PageAdd();return false;">Next</a></li></ul>');
 					showResult();
 					}
+					});
+		}
+	}
+	function searchInCategory()
+	{
+		var surl='https://api.foursquare.com/v2/venues/search?near=chennai&categoryId='+Cid+'&query='+$('#searchInCategory input').val()+'&v=20140806&client_id='+client_id+'&client_secret='+client_secret;
+		$.ajax(surl,{
+				complete:function(xHTTP,status){
+						oData=$.parseJSON(xHTTP.responseText);
+						console.log(oData);
+						startindex=0;
+						$('#showResults tbody').empty();
+						$('body ul').remove();
+						$('body').append('<ul id="movePage" class="pager"><li><a href="#" onClick="PageAdd();return false;">Next</a></li></ul>');
+					showResult();
+						}
 					});
 	}
 	function PageAdd()
